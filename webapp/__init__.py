18ace878adfcfacfb88b86cc3fa2c6593ecaba93
Didod5 <didod1020@gmail.com>
2023-07-02 21:28:46 +0400

    Created templates for main page and product page

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 from webapp.db import db
 
@@ -9,6 +9,20 @@ def create_app():
 
     @app.route('/')
     def index():
-        return 'Start'
+        page_title = 'Aggregator'
+        return render_template('index.html', page_title=page_title)
     
+    @app.route('/product/<int:product_id>')
+    def product(product_id):
+        
+        # здесь надо вытаскивать данные из базы
+        product_data = {
+            'name': '',
+            'description': '',
+            'price': '',
+            'img_path': ''
+        }
+        
+        return render_template('product.html', product=product_data)
+
     return app
