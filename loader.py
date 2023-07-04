e27dab7b3b0fbbead11757c82a6dccd53810d985
rdbone <baysir17@gmail.com>
2023-07-04 20:42:35 +0300

    Modified loader, everything except price

--- loader.py
+++ loader.py
@@ -1,7 +1,8 @@
 from datetime import date
 
 import requests
-from sqlalchemy import insert
+from sqlalchemy import insert, func, select
+from sqlalchemy.exc import IntegrityError
 
 import settings
 from webapp import create_app
@@ -75,8 +76,11 @@ def extract_good_data(good, category):
         final_good['image'] = good['images'][0]
     if 'slug' in good:
         final_good['link'] = settings.PRODUCT_LINK + good['slug']
-    if 'averageDisplay' in good['reviews']['statistics']:
-        final_good['rating'] = good['reviews']['statistics']['averageDisplay']
+    try:
+        if 'averageDisplay' in good['reviews']['statistics']:
+            final_good['rating'] = good['reviews']['statistics']['averageDisplay']
+    except TypeError:
+        final_good['rating'] = None
     if 'description' in good:
         final_good['description'] = good['description']
 
@@ -98,10 +102,17 @@ def get_goods_by_category(category, url, params):
 
 def save_category(category_name):
     category = {'name': category_name}
-    category_id = db.session.scalar(
-        insert(Category).returning(Category.id), category
-    )
-    db.session.commit()
+    try:
+        category_id = db.session.scalar(
+            insert(Category).returning(Category.id), category
+        )
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        result = db.session.execute(
+            select(Category.id).where(Category.name == category['name'])
+        ).first()
+        category_id = result.id
     category["id"] = category_id
     return category
 
@@ -116,12 +127,29 @@ def save_currencies(data):
             }
             unique_currencies.append(currency)
             processed.append(currency['name'])
-    currency_ids = db.session.scalars(
-        insert(Currency).returning(Currency.id, sort_by_parameter_order=True), unique_currencies
-    )
-    db.session.commit()
-    for currency_id, currency in zip(currency_ids, unique_currencies):
-        currency["id"] = currency_id
+    results = db.session.execute(
+        select(Currency).where(Currency.name.in_([currency['name'] for currency in unique_currencies]))
+    ).fetchall()
+    existing_currencies = []
+    if results:
+        for object in results:
+            currency = {
+                'id': object.Currency.id,
+                'name': object.Currency.name,
+                'symbol': object.Currency.symbol
+            }
+            existing_currencies.append(currency)
+    for new_currency in unique_currencies:
+        if new_currency['name'] in [currency['name'] for currency in existing_currencies]:
+            unique_currencies.remove(new_currency)
+    if unique_currencies:
+        currency_ids = db.session.scalars(
+            insert(Currency).returning(Currency.id, sort_by_parameter_order=True), unique_currencies
+        )
+        db.session.commit()
+        for currency_id, currency in zip(currency_ids, unique_currencies):
+            currency["id"] = currency_id
+    unique_currencies += existing_currencies
     return unique_currencies
 
 def save_countries(data):
@@ -129,15 +157,31 @@ def save_countries(data):
     unique_countries = []
     for row in data:
         if row['shop_country'] not in processed:
-            counrty = {'name': row['shop_country']}
-            unique_countries.append(counrty)
-            processed.append(counrty['name'])
-    counrty_ids = db.session.scalars(
-        insert(Country).returning(Country.id, sort_by_parameter_order=True), unique_countries
-    )
-    db.session.commit()
-    for counrty_id, counrty in zip(counrty_ids, unique_countries):
-        counrty["id"] = counrty_id
+            country = {'name': row['shop_country']}
+            unique_countries.append(country)
+            processed.append(country['name'])
+    results = db.session.execute(
+        select(Country).where(Country.name.in_([country['name'] for country in unique_countries]))
+    ).fetchall()
+    existing_countries = []
+    if results:
+        for object in results:
+            country = {
+                'id': object.Country.id,
+                'name': object.Country.name
+            }
+            existing_countries.append(country)
+    for new_country in unique_countries:
+        if new_country['name'] in [country['name'] for country in existing_countries]:
+            unique_countries.remove(new_country)
+    if unique_countries:
+        counrty_ids = db.session.scalars(
+            insert(Country).returning(Country.id, sort_by_parameter_order=True), unique_countries
+        )
+        db.session.commit()
+        for counrty_id, counrty in zip(counrty_ids, unique_countries):
+            counrty["id"] = counrty_id
+    unique_countries += existing_countries
     return unique_countries
 
 def get_counrty_by_id(countries, country_name):
@@ -154,15 +198,35 @@ def save_shops(data, countries):
                 'name': row['shop_name'],
                 'description': row['shop_description'],
                 'site_link': row['shop_link'],
-                'counrty_id': get_counrty_by_id(countries, row['shop_country'])
+                'country_id': get_counrty_by_id(countries, row['shop_country'])
             } 
             unique_shops.append(shop)
-            processed.append(shop['name']) 
-    shop_ids = db.session.scalars(
-        insert(Shop).returning(Shop.id, sort_by_parameter_order=True), unique_shops
-    )
-    for shop_id, shop in zip(shop_ids, unique_shops):
-        shop["id"] = shop_id
+            processed.append(shop['name'])
+    results = db.session.execute(
+        select(Shop).where(Shop.name.in_([shop['name'] for shop in unique_shops]))
+    ).fetchall()
+    existing_shops = []
+    if results:
+        for object in results:
+            shop = {
+                'id': object.Shop.id,
+                'name': object.Shop.name,
+                'description': object.Shop.description,
+                'site_link': object.Shop.site_link,
+                'country_id': object.Shop.country_id
+            }
+            existing_shops.append(shop)
+    for new_shop in unique_shops:
+        if new_shop['name'] in [shop['name'] for shop in existing_shops]:
+            unique_shops.remove(new_shop)
+    if unique_shops:
+        shop_ids = db.session.scalars(
+            insert(Shop).returning(Shop.id, sort_by_parameter_order=True), unique_shops
+        )
+        db.session.commit()
+        for shop_id, shop in zip(shop_ids, unique_shops):
+            shop["id"] = shop_id
+    unique_shops += existing_shops
     return unique_shops
 
 def get_shop_by_id(shops, shop_name):
@@ -187,13 +251,44 @@ def save_goods(data, category_id, shops):
             } 
             unique_goods.append(good)
             processed.append(good['link'])
-    good_ids = db.session.scalars(
-        insert(Good).returning(Good.id, sort_by_parameter_order=True), unique_goods
-    )
-    db.session.commit()
-    for good_id, good in zip(good_ids, unique_goods):
-        good["id"] = good_id
-    return unique_goods
+    results = db.session.execute(
+        select(Good).where(Good.link.in_([good['link'] for good in unique_goods]))
+    ).fetchall()
+    existing_goods = []
+    if results:
+        for object in results:
+            good = {
+                'id': object.Good.id,
+                'name': object.Good.name,
+                'description': object.Good.description,
+                'image': object.Good.image,
+                'link': object.Good.link,
+                'rating': object.Good.rating,
+                'units': object.Good.units,
+                'category_id': object.Good.category_id,
+                'shop_id': object.Good.shop_id
+            }
+            existing_goods.append(good)
+    print('Количество существующих товаров')
+    print(len(existing_goods))
+    print('Количество новых товаров')
+    print(len(unique_goods))
+    existing_links = [good['link'] for good in existing_goods]
+    record_goods = []
+    for new_good in unique_goods: #сделать новый список not in
+        if new_good['link'] not in existing_links: #вынести список в пер до цикла
+            record_goods.append(new_good)
+    print('Количество новых товаров после удаления существующих')    
+    print(len(record_goods))
+    if record_goods:    
+        good_ids = db.session.scalars(
+            insert(Good).returning(Good.id, sort_by_parameter_order=True), record_goods
+        )
+        db.session.commit()
+        for good_id, good in zip(good_ids, record_goods):
+            good["id"] = good_id
+    record_goods += existing_goods
+    return record_goods
 
 def get_currency_by_id(currencies, currency_name):
     for currency in currencies:
@@ -210,14 +305,52 @@ def save_prices(data, currencies, goods):
     for row in data:
         price = {
             'value': row['price'],
-            'value_discount': row.get('price_discount', 0),
+            'value_discount': row.get('price_discount', None),
             'date': row['price_date'],
             'currency_id': get_currency_by_id(currencies, row['currency_name']),
             'good_id': get_good_by_id(goods, row['link'])
-        } 
-        prices.append(price)
-    db.session.execute(insert(Price), prices)
-    db.session.commit()
+        }
+        if price['good_id'] not in [pr['good_id'] for pr in prices]:
+            prices.append(price)
+    subq = select(Price.good_id, func.max(Price.date)).where(Price.good_id.in_([price['good_id'] for price in prices])).group_by("good_id").subquery()
+    results = db.session.execute(
+        select(Price).join(subq, Price.good_id == subq.c.good_id and Price.date == subq.c.date)
+    ).fetchall()
+    existing_prices = []
+    print('Общее количество цен для записи в базу')
+    print(len(prices))
+    if results:
+        for object in results:
+            price = {
+                'id': object.Price.id,
+                'value': object.Price.value,
+                'value_discount': object.Price.value_discount,
+                'date': object.Price.date,
+                'good_id': object.Price.good_id
+            }
+            existing_prices.append(price)
+        if existing_prices:
+            record_price = []
+            existing_good_prices = [price['good_id'] for price in existing_prices]
+            for new_price in prices:
+                if new_price['good_id'] not in existing_good_prices:
+                    record_price.append(new_price)
+                else:
+                    for current_price in existing_prices:
+                        if new_price['good_id'] == current_price['good_id']:
+                            if (new_price['value']!=current_price['value']) or (new_price['value_discount']!= current_price['value_discount']):
+                                record_price.append(new_price)
+            if record_price:
+                print("Количество новых цен для записи")
+                print(len(record_price))
+                db.session.execute(insert(Price), record_price)
+                db.session.commit()
+    else:
+        print('В базе нет цен для таких товаров')
+        db.session.execute(insert(Price), prices)
+        db.session.commit()
+    print('Количество цен, имеющихся в базе')
+    print(len(existing_prices))
 
 def save_category_items(category_name, category_items):
     app = create_app()
